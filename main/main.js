7a0550673eb6304ec68a51a9ecf519b82d714e71
hillium <maruruku@stu.csust.edu.cn>
2019-05-21 16:59:12 +0800

    completed.

--- main/main.js
+++ main/main.js
@@ -1,4 +1,25 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
+function fromEntries(entries) {
+    let result = {};
+    for (let [k, v] of entries) {
+        result[k] = v;
+    }
+    return result;
+}
+
+function arrayToLineMapping(items) {
+    return fromEntries(items.map((item, idx) => [idx, item]));
+}
+
+const line1 = arrayToLineMapping("._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.".split(/\s+/));
+const line2 = arrayToLineMapping("|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|".split(/\s+/));
+const line3 = arrayToLineMapping("|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|".split(/\s+/));
+
+function toLEDString(n) {
+    const stringNumber = String(n);
+    const chars = Array.from(stringNumber);
+    return [line1, line2, line3].map(line => 
+        chars.map(c => line[c]).join(' ')
+    ).join('\n'); 
+}
+
+module.exports = n => toLEDString(n) + '\n'
